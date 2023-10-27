a442a4d494743c2b7d08cd919a18bdf6bd4706a1
Flor Muñoz <florenciamlombardi@gmail.com>
2023-10-27 18:26:20 -0300

    Reporte html y ejecutador de scripts

---
+++ EjecutarScripts.bat
@@ -0,0 +1,5 @@
+Código:
+echo. ##############TEST PATH #############
+cd ./Sprint3
+python -m pytestTC1091.py TC1093.py TC1096.py --html=../result/FlorenciaMunoz.html --self-contained-html
+start FlorenciaMunoz.html
